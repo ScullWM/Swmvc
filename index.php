2e189d275c1285442ef514dbd44be74c14d77620
ScullWM <mistralconnexion@gmail.com>
2012-07-16 22:42:08 +0200

    Fear factoring

--- index.php
+++ index.php
@@ -5,40 +5,35 @@
  *
  */
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+ini_set('error_reporting', E_ALL);
 
-//require('config.php');
 require('model/core.php');
 require('controllers/core.php');
 
-
 // here we include all our external class
 // ask it if you need it
-
 foreach(glob("lib/*.class.php") as $f):
-    require_once($f);
+    require_once $f;
 endforeach;
 
 include("views/header.tmpl"); // header is simply include there
 
-if (isset($_GET['c']) && isset($_GET['a'])) {
+if (isset($_GET['c'])) {
     
     $controller_file = 'controllers/'.$_GET['c'].'.php';
 
     if(file_exists($controller_file)){
         require_once $controller_file;  
         // Constroller instanciation
-        $controleur = $_GET['c'];
-        $controleur = new $controleur();
+        $controller = new $_GET['c']();
 
         // Get Action and Id
         if(isset($_GET['a'])){   $action = $_GET['a'];  }else { $action = 'index'; }
         if(isset($_GET['id'])){  $id     = $_GET['id']; }
 
         // does the action existe in the controller asked ?
-        if (method_exists($controleur, $action)) {
-            $controleur->$action();
+        if (method_exists($controller, $action)) {
+            $controller->$action();
         }else {
             $error = 'This action does not exist in this controller';
             require_once 'views/error.tmpl';
